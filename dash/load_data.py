17a8adc6803bda7d969c404cf3badf5af639fece
Lucas Rodrigues <lucasrodriguespimentel@gmail.com>
2023-11-30 09:51:02 -0300

    Update load_data.py

--- dash/load_data.py
+++ dash/load_data.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import glob
 import os
+from dotenv import load_dotenv
+load_dotenv('../.env')
+
+VIEWS_PROTOCOL = os.environ.get('VIEWS-PROTOCOL', 'http')
+VIEWS_HOST = os.environ.get('VIEWS-FETCH-HOST', 'localhost')
 
 # Function to load data and calculate the initial graphs
 def load_data(data_viz_list):
     malaria_views = {}
     for malaria_viz in data_viz_list:
-        malaria_views[malaria_viz] = pd.read_csv('http://52.206.171.26/api-poc/fetch/' + malaria_viz.split('_')[-1])
+        malaria_views[malaria_viz] = pd.read_csv('{}://{}/views-api/fetch/'.format(VIEWS_PROTOCOL, VIEWS_HOST) + malaria_viz.split('_')[-1])
         if 'variable' in malaria_views[malaria_viz].columns:
             malaria_views[malaria_viz] = malaria_views[malaria_viz].pivot_table(index='ano', columns='variable', values='count', aggfunc='sum')
     return malaria_views
