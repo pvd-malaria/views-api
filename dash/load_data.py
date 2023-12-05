5d049edfb2683fba5f2de3d10816d342605da9a7
Lucas Rodrigues <lucasrodriguespimentel@gmail.com>
2023-12-05 10:40:38 -0300

    Update load_data.py

--- dash/load_data.py
+++ dash/load_data.py
@@ -4,14 +4,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-VIEWS_PROTOCOL = os.environ.get('VIEWS_PROTOCOL', 'http')
-VIEWS_HOST = os.environ.get('VIEWS_FETCH_HOST', 'localhost')
+VIEWS_ADDRESS = os.environ.get('VIEWS_ADDRESS', 'promalaria-views-api:9000')
 
 # Function to load data and calculate the initial graphs
 def load_data(data_viz_list):
     malaria_views = {}
     for malaria_viz in data_viz_list:
-        malaria_views[malaria_viz] = pd.read_csv('{}://{}:8889/fetch/'.format(VIEWS_PROTOCOL, VIEWS_HOST) + malaria_viz.split('_')[-1])
+        malaria_views[malaria_viz] = pd.read_csv('{}/fetch/'.format(VIEWS_ADDRESS) + malaria_viz.split('_')[-1])
         if 'variable' in malaria_views[malaria_viz].columns:
             malaria_views[malaria_viz] = malaria_views[malaria_viz].pivot_table(index='ano', columns='variable', values='count', aggfunc='sum')
     return malaria_views
