26b6a68af578839b7f7e5b3eeaa4255f0e174e4d
Lucas Rodrigues <lucasrodriguespimentel@gmail.com>
2023-12-05 10:48:00 -0300

    Update load_data.py

--- dash/load_data.py
+++ dash/load_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 import glob
 import os
 from dotenv import load_dotenv
@@ -10,7 +11,11 @@ VIEWS_ADDRESS = os.environ.get('VIEWS_ADDRESS', 'promalaria-views-api:9000')
 def load_data(data_viz_list):
     malaria_views = {}
     for malaria_viz in data_viz_list:
-        malaria_views[malaria_viz] = pd.read_csv('{}/fetch/'.format(VIEWS_ADDRESS) + malaria_viz.split('_')[-1])
+        url = '{}/fetch/'.format(VIEWS_ADDRESS) + malaria_viz.split('_')[-1]
+        r = requests.get(url)
+        with open('/tmp/output.csv', 'wb') as f:
+            f.write(r.content)
+        malaria_views[malaria_viz] = pd.read_csv('/tmp/output.csv')
         if 'variable' in malaria_views[malaria_viz].columns:
             malaria_views[malaria_viz] = malaria_views[malaria_viz].pivot_table(index='ano', columns='variable', values='count', aggfunc='sum')
     return malaria_views
